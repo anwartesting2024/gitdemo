4f215816a51edca528ddd007a26241abb6f1394e
moanwar <anwartest1@gmail.com>
2024-05-14 22:50:43 -0400

    Modified Hello and added test files

---
+++ AppiumTest.java
@@ -0,0 +1,12 @@
+public class AppiumTest{
+	@Test
+	public void postJira(){
+		System.out.println("Post Jira1")
+	
+	}
+	@Test
+	public void deleteTwitter(){
+		System.out.println("Delete Twiter");
+	}
+
+}
--- HelloProgram.java
+++ HelloProgram.java
@@ -3,5 +3,18 @@ class HelloProgram{
 		System.out.println("Hello program")
 		System.out.println("Hello Java program");
 		System.out.println("Hello Python program");
+		
+		addMethod(5,7);
+		subtractMethod(9,5);
+		
+		
+	}
+	
+	public static int addMethod(int a, int b){
+		System.out.println("Sum:"a+b);
+	}
+	
+	public static int subtractMethod(int a, int b){
+		System.out.println("Subtraction result:" a-b);
 	}
 }
