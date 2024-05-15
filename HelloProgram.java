ee261086e281cdf0ae9a22f80418dac98b9a7f96
Kamrul <bb.test1005@gmail.com>
2024-05-14 22:26:49 -0400

    Two lines by GitKamrul

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,5 +1,7 @@
 class HelloProgram{
 	public void static main(arg){
 		System.out.println("Hello program")
+		System.out.println("Hello Java program");
+		System.out.println("Hello Python program");
 	}
 }
